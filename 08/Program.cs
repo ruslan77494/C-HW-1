10cd7029bf43eb7629ba719e73f8409c4d0c977c
ruslan <rus.pro94@bk.ru>
2022-05-04 18:19:44 +0500

    T08

---
+++ 08/Program.cs
@@ -0,0 +1,17 @@
+﻿// задача 4 Напишите программу которая принимает на вход число (N)  а на выходе принимает все четные числа от 1 до N.
+
+Console.WriteLine(" Пожалуйста введите число: ");
+int n= Convert.ToInt32(Console.ReadLine());
+for (int i = 1 ; i <= n; i++)
+{
+    
+    if (i % 2 == 0)
+    {
+        Console.Write(i);
+    }
+
+     {
+        Console.Write(" ");
+    }
+}
+
