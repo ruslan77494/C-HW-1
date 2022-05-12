29105957fe8dd4dbd93d02240cc0338023fc9c3e
ruslan <rus.pro94@bk.ru>
2022-05-12 20:14:51 +0500

    03

---
+++ 03/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите прогромму которая на вход принимает три  числа и выдает какое число больше .
+
+Console.WriteLine(" Пожалуйста введите 3 числа : ");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = Convert.ToInt32(Console.ReadLine());
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+int a = n;
+
+int b = n1;
+
+int c = n2;
+
+int max = 0;
+
+if (b > max) max = b;
+
+if (c > max) max = c;
+
+if (a > max)  max = a; 
+
+Console.WriteLine(max);
+
+
