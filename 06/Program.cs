865c3039240f0fa3db66623d909fbb88b8b37402
ruslan <rus.pro94@bk.ru>
2022-05-12 21:10:25 +0500

    06

---
+++ 06/Program.cs
@@ -0,0 +1,15 @@
+﻿
+    // Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+Console.WriteLine("Введите пожалуйста  число  : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n == 0) {
+    Console.WriteLine (" ");
+}
+else
+if (n % 2 == 0) {
+    Console.WriteLine(" + ");
+}
+else 
+Console.WriteLine(" - " );
+
